acc7040b6dc6de3fb04dfaed042a74faa658cea5
Jefté Lima <jefte_lima@hotmail.com>
2023-11-03 09:59:11 -0300

    Pull Request Preview

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,20 @@ export default function HomeScreen() {
   return (
     <div>
       <h1>Página Inicial</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit
+        amet sodales eros, ut iaculis lectus. Nullam varius eget quam eget
+        tempor. Ut nec lectus pellentesque, mollis arcu viverra, elementum
+        sapien. Sed mollis sapien sit amet pellentesque rhoncus. Vestibulum
+        ultricies diam ac justo hendrerit ornare. Pellentesque blandit leo vel
+        hendrerit suscipit. Donec vitae risus volutpat, malesuada nisi ac,
+        finibus leo. Maecenas vitae sem semper, interdum neque et, tincidunt
+        quam. Sed ultricies orci quis vehicula commodo. Cras nisi dolor, dictum
+        sit amet vulputate at, pharetra et tortor. Cras sed vestibulum dui.
+        Vestibulum eu orci in urna laoreet feugiat at a felis. Proin vitae
+        pulvinar nulla. Nulla bibendum purus sodales sapien vulputate dignissim.
+        Duis dignissim magna velit.
+      </p>
       <Link href="/sobre">Sobre</Link>
     </div>
   );
--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,1 +1,12 @@
-console.log("Olá mundo");
+import { execSync } from "child_process";
+
+console.log("[DEPLOY_PREVIEW]: START");
+const command = "yarn deploy:staging";
+const output = execSync(command, { encoding: "utf-8" });
+const outputLines = output.split("/n");
+const DEPLOY_URL = outputLines[outputLines.length - 1];
+console.log("[DEPLOY_PREVIEW]: END");
+console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
+
+/* console.log("[GITHUB_COMMENT]: START");
+console.log("[GITHUB_COMMENT]: END"); */
