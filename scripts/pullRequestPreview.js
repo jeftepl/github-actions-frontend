604ec4d9acd0989ed9e7591494b46b05cd01d89c
Jefté Lima <jefte_lima@hotmail.com>
2023-11-03 11:37:33 -0300

    Update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -8,9 +8,12 @@ const outputLines = output.split("/n");
 console.log("outputLines: ", outputLines);
 const packageJson = JSON.parse(readFileSync("package.json", "utf8"));
 const projectName = packageJson.name;
-const DEPLOY_URL = outputLines.find(
+/* const DEPLOY_URL = outputLines.find(
   (line) => line.includes("https://") && line.includes(projectName),
-);
+); */
+const DEPLOY_URL = outputLines
+  .filter((line) => line.includes("https://"))
+  .pop();
 console.log("[DEPLOY_PREVIEW]: END");
 
 console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
