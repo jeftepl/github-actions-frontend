ad7ebcff2072712afd1cf16e7f59312dfa4e5e7a
Jefté Lima <jefte_lima@hotmail.com>
2023-11-03 11:03:02 -0300

    Update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -4,9 +4,11 @@ console.log("[DEPLOY_PREVIEW]: START");
 const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: "utf-8" });
 const outputLines = output.split("/n");
-const DEPLOY_URL = outputLines[outputLines.length - 1];
+const packageJson = JSON.parse(readFileSync("package.json", "utf8"));
+const projectName = packageJson.name;
+const DEPLOY_URL = outputLines.find((line) => line.includes(projectName));
 console.log("[DEPLOY_PREVIEW]: END");
-console.log("output lines: ", outputLines.length);
+
 console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
 
 /* console.log("[GITHUB_COMMENT]: START");
