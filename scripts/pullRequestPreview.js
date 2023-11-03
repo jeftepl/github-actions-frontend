194433cc71b2655ae2320d6cd7e1675c0b0912a4
Jefté Lima <jefte_lima@hotmail.com>
2023-11-03 10:29:52 -0300

    Update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -6,7 +6,8 @@ const output = execSync(command, { encoding: "utf-8" });
 const outputLines = output.split("/n");
 const DEPLOY_URL = outputLines[outputLines.length - 1];
 console.log("[DEPLOY_PREVIEW]: END");
-console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
+console.log(outputLines);
+//console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
 
 /* console.log("[GITHUB_COMMENT]: START");
 console.log("[GITHUB_COMMENT]: END"); */
