8e2edfcaa9a6fe1e2bd3a0c7e610386337e746e7
Jefté Lima <jefte_lima@hotmail.com>
2023-11-04 11:37:00 -0300

    Update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -21,35 +21,6 @@ const GH_COMMENT = `
 
 const GITHUB_REPO = GITHUB_REPOSITORY.split("/").pop();
 
-const defaultHeaders = {};
-defaultHeaders["authorization"] = `token ${GITHUB_TOKEN}`;
-defaultHeaders["accept"] =
-  "application/vnd.github.v3+json; application/vnd.github.antiope-preview+json";
-defaultHeaders["content-type"] = "application/json";
-const url = `https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/issues/${GITHUB_PR_NUMBER}/comments`;
-console.log("GITHUB_REPOSITORY", GITHUB_REPOSITORY);
-console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
-
-fetch(url, {
-  headers: defaultHeaders,
-  method: "POST",
-  body: JSON.stringify({
-    body: GH_COMMENT,
-  }),
-})
-  .then((response) => {
-    if (response.ok) return response.json();
-    throw new Error(response.statusText);
-  })
-  .catch((err) => {
-    console.log("[COMMENT_ON_GITHUB: ERROR]");
-    throw new Error(err);
-  })
-  .finally(() => {
-    console.log("[COMMENT_ON_GITHUB: END]");
-  });
-
-/*
 console.log("GITHUB_OWNER", GITHUB_OWNER);
 console.log("GITHUB_REPOSITORY", GITHUB_REPO);
 console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
@@ -80,7 +51,7 @@ fetch(url, {
   })
   .finally(() => {
     console.log("[COMMENT_ON_GITHUB: END]");
-  }); */
+  });
 
 // Octokit.js
 /* const octokit = new Octokit({
