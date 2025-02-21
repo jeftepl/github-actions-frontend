2eb4d97b6ad0ea9e8b0a8976fb8b74b6705b2b9a
Jefté Lima <jefte_lima@hotmail.com>
2025-02-21 08:47:18 -0300

    chore: migrate codebase to ES modules
    Update project configuration and scripts to use ES modules (ESM) format:
    - Configure package.json with "type": "module" for native ESM support
    - Convert next.config.js to use ES module export syntax
    - Refactor pullRequestPreview.js to use ES modules and @octokit/core library
    
    This migration improves code consistency and modernizes the project structure
    by adopting the standard ES modules format across the codebase.

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,5 +1,5 @@
-const { execSync } = require("child_process");
-//const { Octokit } = require("@octokit/core");
+import { execSync } from "child_process";
+import { Octokit } from "@octokit/core";
 
 console.log("[DEPLOY_PREVIEW]: START");
 const command = "yarn deploy:staging";
@@ -11,7 +11,6 @@ console.log("[DEPLOY_PREVIEW]: END");
 console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
 
 // GitHub Pull Request Comment
-
 console.log("[GITHUB_COMMENT]: START");
 const { GITHUB_OWNER, GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_PR_NUMBER } =
   process.env;
@@ -25,36 +24,7 @@ console.log("GITHUB_OWNER", GITHUB_OWNER);
 console.log("GITHUB_REPOSITORY", GITHUB_REPO);
 console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
 
-const headers = {};
-headers["Accept"] = "application/vnd.github+json";
-headers["Authorization"] = `Bearer ${GITHUB_TOKEN}`;
-headers["X-GitHub-Api-Version"] = "2022-11-28";
-headers["Content-Type"] = "application/json";
-const url = `https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/issues/${GITHUB_PR_NUMBER}/comments`;
-const body = JSON.stringify({
-  body: GH_COMMENT,
-});
-
-fetch(url, {
-  method: "POST",
-  headers,
-  body,
-})
-  .then(async (response) => {
-    if (response.ok) return response.json();
-    const text = await response.text();
-    throw new Error(text);
-  })
-  .catch((err) => {
-    console.log("[COMMENT_ON_GITHUB: ERROR]");
-    throw new Error(err);
-  })
-  .finally(() => {
-    console.log("[COMMENT_ON_GITHUB: END]");
-  });
-
-// Octokit.js
-/* const octokit = new Octokit({
+const octokit = new Octokit({
   auth: GITHUB_TOKEN,
 });
 
@@ -78,4 +48,4 @@ fetch(url, {
   } finally {
     console.log("[COMMENT_ON_GITHUB: END]");
   }
-})(); */
+})();
